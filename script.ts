f687e112fc158e85fe013e406e9858c915039b68
Muhammad Awais <awaistopper20@gmail.com>
2023-07-07 21:45:16 +0500

    Prisma client basics

---
+++ script.ts
@@ -0,0 +1,21 @@
+import { PrismaClient } from "@prisma/client";
+
+const prisma = new PrismaClient();
+
+async function main() {
+  //   const user = await prisma.user.create({
+  //     data: {
+  //       name: "Alice",
+  //     },
+  //   });
+  const users = await prisma.user.findMany();
+  console.log(users);
+}
+
+main()
+  .catch((e) => {
+    console.error(e);
+  })
+  .finally(async () => {
+    await prisma.$disconnect();
+  });
